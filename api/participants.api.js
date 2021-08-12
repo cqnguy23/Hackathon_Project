09c902fbb96d53b97750da6f520cac0a3f5e626e
luke <lukehoang195@gmail.com>
2021-08-12 17:44:10 +0700

    add bank info, add generate token for user

--- api/participants.api.js
+++ api/participants.api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const participants = require("../controllers/participants.controller");
-
+//when i create participants, they need to belong to a petition
 router.post("/", participants.create);
 router.get("/", participants.read);
 router.put("/:id", participants.update);
--- models/User.model.js
+++ models/User.model.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const jwt = require("jsonwebtoken");
+//remove this later and add to env
+const JWT_SECRET_KEY = "jaguar";
 const userSchema = Schema(
   {
     firstName: String,
     lastName: String,
+    phone: Number,
+    password: String,
     petitions: [{ type: Schema.ObjectId, ref: "Petition" }],
     participants: [{ type: Schema.ObjectId, ref: "Participant" }],
     gender: {
@@ -22,8 +26,15 @@ const userSchema = Schema(
   },
   {
     timestamps: true,
-  },
+  }
 );
+
+userSchema.methods.generateToken = async function () {
+  const accessToken = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return accessToken;
+};
 
 const User = mongoose.model("User", userSchema);
 
