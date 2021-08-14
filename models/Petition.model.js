3944bffc199917b494441a6345bb837a8ea1ec95
luke <lukehoang195@gmail.com>
2021-08-14 11:39:16 +0700

    untrack .env

--- models/Petition.model.js
+++ models/Petition.model.js
@@ -14,8 +14,6 @@ const petitionSchema = Schema(
       default: 0,
       type: Number,
     },
-    description: String,
-    distance: Number,
     type: {
       type: String,
       enum: {
@@ -44,6 +42,7 @@ const petitionSchema = Schema(
     description: String,
     images: [{ imageUrl: String }],
     items: [{ ref: "Item", type: Schema.ObjectId }],
+    items: [{ type: Schema.ObjectId, ref: "Item" }],
     participants: [{ type: Schema.ObjectId, ref: "Participant" }],
     bankInfo: {
       bankName: String,
